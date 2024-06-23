169291071b907ee03e8fd5707523c04a34d69dd9
sondavid85 <sondavid85@yahoo.com>
2024-06-23 14:08:56 -0700

    update conn to use host and port from env

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'mysql',
-    port: 3306,
+    port: process.env.DB_PORT,
   }
 );
 
